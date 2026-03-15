38b97131b6c572d065d3f8c6a6c36ef6937e4238
heydaytime <whynotmihir@gmail.com>
2026-03-15 01:09:27 -0500

    changes

--- commands/get_MIT.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-gh api repos/heydaytime/licenses/contents/MIT \
-  -H "Accept: application/vnd.github.v3.raw" \
-  > LICENSE
-
--- commands/run_make.zsh
+++ commands/run_make.zsh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-go run .
+make run
